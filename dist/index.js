47f20b30afe203dc660aa0d060cafc40567848fd
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-30 21:59:08 +0100

    Initial implementation

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,37 @@ Object.defineProperty(exports, "__esModule", {
 exports.activate = activate;
 exports.deactivate = deactivate;
 
+var _electron = require("electron");
+
+let listener = null;
+
 function activate() {
-  console.log('Plugin activated');
+  if (!inkdrop.isMainWindow) {
+    return;
+  }
+
+  const lastProcessId = inkdrop.config.get('minimize-on-launch.pid') || -1;
+  const currentProcessId = _electron.remote.process.pid; // If the process id is the same as the last saved id, we can assume the
+  // window was simply reloaded. While there is a chance for the pid to be the
+  // same during restarts, there is no better way to differentiate between an
+  // app restart and a window reload.
+
+  if (lastProcessId === currentProcessId) {
+    return;
+  }
+
+  inkdrop.config.set('minimize-on-launch.pid', currentProcessId); // The window is not always restored to the correct size if it is minimized
+  // before the app is ready.
+
+  listener = inkdrop.onAppReady(() => {
+    inkdrop.window.minimize();
+  });
 }
 
 function deactivate() {
-  console.log('Plugin deactivated');
+  if (listener !== null) {
+    listener.dispose();
+    listener = null;
+  }
 }
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
+import { remote } from 'electron';
+
+let listener = null;
+
 export function activate() {
-  console.log('Plugin activated');
+  if (!inkdrop.isMainWindow) {
+    return;
+  }
+
+  const lastProcessId = inkdrop.config.get('minimize-on-launch.pid') || -1;
+  const currentProcessId = remote.process.pid;
+
+  // If the process id is the same as the last saved id, we can assume the
+  // window was simply reloaded. While there is a chance for the pid to be the
+  // same during restarts, there is no better way to differentiate between an
+  // app restart and a window reload.
+  if (lastProcessId === currentProcessId) {
+    return;
+  }
+
+  inkdrop.config.set('minimize-on-launch.pid', currentProcessId);
+
+  // The window is not always restored to the correct size if it is minimized
+  // before the app is ready.
+  listener = inkdrop.onAppReady(() => {
+    inkdrop.window.minimize();
+  });
 }
 
 export function deactivate() {
-  console.log('Plugin deactivated');
+  if (listener !== null) {
+    listener.dispose();
+    listener = null;
+  }
 }
